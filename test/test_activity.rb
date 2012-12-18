540a5acaadeadce0e4377bae0dd951036b3fc26b
Florian Matter <florianmatter@gmail.com>
2012-12-18 20:59:31 +0100

    extended activity tests
    now testing more than 50% of activity.

--- test/test_activity.rb
+++ test/test_activity.rb
@@ -2,25 +2,75 @@ class MyTest < Test::Unit::TestCase
 
   def setup
     User.delete_all
+    @user = User.init(:name => "user1", :password => "Zz!45678")
+    @time = Time.now
+    @i = {:name => "test item"}
+    def @i.name
+      return self[:name]
+      end
   end
 
-  def test_attributes
-    user = User.init(:name => "user1", :password => "Zz!45678")
-    time = Time.now
-    a = {:name => "test item"}
-    def a.name
-      return self[:name]
-    end
-    act = Activity::new_comment_activity(user, a)
-    assert(act.message.message_ary[2] == "#{a.name}") # message_ary supposed to be ["COMMENTED_ON", " ", item.name.clone]
-    assert(act.creator == user)
+  def test_comment_activity
+    act = Activity::new_comment_activity(@user, @i)
+    assert(act.message.message_ary[2] == "#{@i.name}") # message_ary supposed to be ["COMMENTED_ON", " ", item.name.clone]
+    assert(act.creator == @user)
     assert(act.type == :comment)
-    assert(time - act.timestamp > -0.00009) # this might fail sometimes if ruby feels like it
-
+    assert(@time - act.timestamp > -0.00009) # this might fail sometimes if ruby feels like it
     # assert cannot change attributes
     assert_raise NoMethodError do
       act.type = :test
     end
   end
+
+  def test_item_activity
+    act = Activity::new_createitem_activity(@user, @i)
+    assert(act.message.message_ary[2] == "#{@i.name}") # message_ary supposed to be ["CREATED_ITEM", " ", item.name.clone]
+    assert(act.creator == @user)
+    assert(act.type == :createitem)
+    assert(@time - act.timestamp > -0.00009) # this might fail sometimes if ruby feels like it
+                                                       # assert cannot change attributes
+    assert_raise NoMethodError do
+      act.type = :test
+    end
+  end
+
+  def test_follow_activity
+    @follow =  User.init(:name => "FollowMe", :password => "Zz!45678")
+    act = Activity::new_follow_activity(@user, @follow)
+    assert(act.message.message_ary[1] == " FollowMe") # message_ary supposed to be ["FOLLOWS", " ", follow.name]
+    assert(act.creator == @user)
+    assert(act.type == :follow)
+    # assert cannot change attributes
+    assert_raise NoMethodError do
+      act.type = :test
+    end
+  end
+
+  def test_activate_activity
+    act = Activity::new_activate_activity(@user, @i)
+    assert(act.message.message_ary[2] == "#{@i.name}") # message_ary supposed to be ["ACTIVATED_ITEM", " ", item.name.clone]
+    assert(act.creator == @user)
+    assert(act.type == :activate)
+    assert(@time - act.timestamp > -0.00009) # this might fail sometimes if ruby feels like it
+                                                       # assert cannot change attributes
+    assert_raise NoMethodError do
+      act.type = :test
+    end
+  end
+
+  def test_buy_activity
+    act = Activity::new_buy_activity(@user, @i)
+    assert(act.message.message_ary[2] == "#{@i.name}") # message_ary supposed to be ["BOUGHT_ITEM", " ", item.name.clone]
+    assert(act.creator == @user)
+    assert(act.type == :buy)
+    assert(@time - act.timestamp > -0.00009) # this might fail sometimes if ruby feels like it
+                                                       # assert cannot change attributes
+    assert_raise NoMethodError do
+      act.type = :test
+    end
+  end
+
+
+
 
 end
